3648e7f77f6a5a5d153b2ccf66a82e220e7bf918
Ubuntu <ubuntu@ip-172-31-8-120.ap-southeast-1.compute.internal>
2021-03-16 07:37:13 +0000

    pivot env variable

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,28 @@
 package io.pivotal.pal.tracker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+
+    private String hello;
+
+    public WelcomeController( @Value("${welcome.message}") String message) {
+        this.hello = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return this.hello;
+    }
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
     }
 }
